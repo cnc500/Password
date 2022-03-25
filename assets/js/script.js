7e9ba48d28619396151a8bbf71ba5dfee32b6df2
Christian Carnevale <cncarnevale@gmail.com>
2022-03-25 19:46:58 -0400

    additional js code written, html file properly linked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
 var lowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers=["0","1","2","3","4","5","6","7","8","9"];
-var specialChar=["!","@","#","$","%","^","&","(",")","*";
-var password=[lowercaseFirst,uppercaseFirst,numberFirst,specialCharFirst];
-var dataNumber=[4];
-var inputNumberChar=[numberChar]
+var specialChar=["!","@","#","$","%","^","&","(",")","*"];
+
+var dataNumber=4;
+
 var generateBtn = document.querySelector("#generate");
 
-function firstChar() {
 
 
-}
+
 
 
 
@@ -22,38 +21,78 @@ function firstChar() {
 
 // Write password to the #password input
 function writePassword() { //Their code
-  var password = generatePassword(); //Their code  should there be a {} instead of ; ???
+  console.log("test");
+   // var password = generatePassword(); //Their code  should there be a {} instead of ; ???
 
 // generate random lowercase letter
-  var randomNumber1=Math.floor(Math.random()+26);
+  var randomNumber1 = Math.floor(Math.random()*26);
   var lowercaseFirst=lowerCase[randomNumber1];
 // generate random uppercase letters
-  var randomNumber2=Math.floor(Math.random()+26);
+  var randomNumber2 = Math.floor(Math.random()*26);
   var uppercaseFirst=upperCase[randomNumber2];
 // generate random numbers
-  var randomNumber3=Math.floor(Math.random()+10);
-  var numberFirst=numbers[randomNumber3];
+  var randomNumber3 = Math.floor(Math.random()*10);
+  var numberFirst = numbers[randomNumber3];
 // generate random special characters
-  var randomNumber4=Math.floor(Math.random()+10);
-  var specialCharFirst=numbers[randomNumber4];
+  var randomNumber4 = Math.floor(Math.random()*10);
+  var specialCharFirst = specialChar[randomNumber4];
 // first character possibilities of password
-  var password=[lowercaseFirst,uppercaseFirst,numberFirst,specialCharFirst];
+  var password = [lowercaseFirst,uppercaseFirst,numberFirst,specialCharFirst];
 // prompt for using lowercase letters
-
+  var useLowerCase = window.confirm ("Would you like to use lowercase letters in your password?  If not, press cancel.");
 // if not lowercase than delete lowercaseFirst and decrease dataTypes by 1
+    if (!useLowerCase) {
+      dataNumber = dataNumber - 1;
+      password.shift();
+      console.log(password);
+    }
 
 // prompt for using uppercase letters
-
-// if not uppercase than delete uppercaseFirst and decrease dataTypes by 1
-
+  var useUpperCase = window.confirm ("Would you like to use uppercase letters in your password?  If not, press cancel.");
+// if not uppercase than delete uppercaseFirst from password array and decrease dataTypes by 1
+  if (!useUpperCase) {
+      dataNumber = dataNumber - 1;
+      if (!useLowerCase) {
+        password.shift();
+      } else {
+        password = password.splice(1,1);
+        }
+  }  
 // prompt for using numbers
-
-// if not number tha delete numberFirst and decrease dataTypes by 1
-
+  var useNumbers = window.confirm ("Would you like to use numbers in your password?  If not, press cancel.");
+// if no numbers, than delete numberFirst and decrease dataTypes by 1
+        if (!numbers) {
+          dataNumber = dataNumber - 1;
+        
+          if (!useUpperCase && !useLowerCase) {
+            password.shift();
+          } else if (!useUpperCase || !useLowerCase) {
+              password = password.splice(1,1);
+            }
+            else {
+              password = password.splice(2,1);
+            }
+        }
 // if lowercase, uppercase, or number is true, 
 //   prompt for using special characters, 
 //   generate random character from random characters in password array
+// var inputNumberChar=[numberChar]
 //   else alert using special char to generate pw and go to for loop to generate remaining characters
+  var useSpecialChar = false;
+  if (useLowerCase || useUpperCase || useNumbers) {
+    useSpecialChar = window.confirm ("Would you like to use special characters in your password?  If not, press cancel.");
+  }
+  else {
+    window.alert("Your password will be generated using only special characters.");
+    
+  }
+    if (!useSpecialChar) {
+      dataNumber = dataNumber - 1;
+      password.pop();
+    }
+    
+        
+
 
 // for loop using i=1; x < numberChar; i++ adding random character to password array in each iteration
 
