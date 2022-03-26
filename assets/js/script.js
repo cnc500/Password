2a65edbcf37e8e360827172af6f0ae3339cddb2f
Christian Carnevale <cncarnevale@gmail.com>
2022-03-26 10:04:42 -0400

    put dataNumber under fuction (should not be global), set useSpecialChar to true in logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,13 @@ var lowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","
 var upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers=["0","1","2","3","4","5","6","7","8","9"];
 var specialChar=["!","@","#","$","%","^","&","(",")","*"];
-var dataNumber=4;
+//datanumber (number of types of characters plus 1) is 5 for math to work in generating random characters.  This 
+//number is reduced as user declines types of characters.
+
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() { //Their code
+  var dataNumber=4;
   console.log("test");
 // generate random lowercase letter
   var randomNumber1 = Math.floor(Math.random()*26);
@@ -55,6 +58,7 @@ function writePassword() { //Their code
               passwordData.splice(2,1);
             }
         }
+        console.log(dataNumber);
         console.log(passwordData); 
 // if lowercase, uppercase, or number is true, 
 //   prompt for using special characters, 
@@ -71,9 +75,11 @@ function writePassword() { //Their code
     }
   }
   else {
+    useSpecialChar = true;
     window.alert("Your password will be generated using only special characters.");
     }
-    console.log(passwordData)
+    console.log(dataNumber);
+    console.log(passwordData);
 // Create random first character of password from up to four random that may include a lowercase letter, an uppercase letter, 
 // a number and a special character. 
   randomNumber = Math.floor(Math.random()*dataNumber);
@@ -87,6 +93,11 @@ function writePassword() { //Their code
 
   for (i=1;i<numberPasswordChar;i++) {
     randomNumber = Math.floor(Math.random()*dataNumber);
+   /* if (useLowerCase) {
+      var randomNumber5 = Math.floor(Math.random()*26);
+      lowercaseFirst=lowerCase[randomNumber5]; 
+    } */
+    console.log(passwordData)
 // Generates next password character
     var nextPasswordChar = passwordData[randomNumber];
     password.push(nextPasswordChar);
